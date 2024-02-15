d997d26b890cb75fe35d9de6a02a8b604dd58e2c
munaimun <munaimunz@gmail.com>
2024-02-15 18:26:43 +0600

    created the category elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import regular from './assets/img/regular.jpg'
+import adventure from './assets/img/adventure.jpg'
+import space from './assets/img/space.jpg'
+import special from './assets/img/speacial.jpg'
+import beach from './assets/img/beach.jpg'
+
+
+const categories = [
+  {
+    id: 1,
+    title: "Regular",
+    img: regular,
+  },
+  {
+    id: 2,
+    title: "Astronomy",
+    img: space,
+    
+  },
+  {
+    id: 3,
+    title: "Special-Edition",
+    img: special,
+  },
+  {
+    id: 4,
+    title: "Beach",
+    img: beach,
+  },
+  {
+    id: 5,
+    title: "Adventure",
+    img: adventure,
+  },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      {categories.map( ({title, id, img}) => (
+        <div key={id} className='category-container'>
+          <div className='background-image'> <img src={img} />
+          </div>
+        <div className='category-body-container'>
+
+          <h2>{title}</h2>
+          <p>shoop Now</p>
+        </div>
+      </div>
+      ) )}
     </div>
   );
 }
